3b6ff5b13f2bb954f5a3121abddd108113d9f702
Zachary Becknell <zbecknell@users.noreply.github.com>
2020-02-21 12:53:09 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlazorWasmTest
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddSingleton<TestAuthenticationStateProvider>();
+            builder.Services.AddSingleton<AuthenticationStateProvider>(s =>
+            {
+                return s.GetRequiredService<TestAuthenticationStateProvider>();
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ TestAuthenticationStateProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlazorWasmTest
+{
+    public class TestAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        static AuthenticationState SignedOutAuthenticationState => new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+        {
+            new Claim(ClaimTypes.Name, "username")
+        })));
+
+        AuthenticationState _authenticationState = SignedOutAuthenticationState;
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            return Task.FromResult(_authenticationState);
+        }
+
+        public void SignIn()
+        {
+            var signedInIdentity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, "username")
+            }, "someAuthTypeName");
+
+            _authenticationState = new AuthenticationState(new ClaimsPrincipal(signedInIdentity));
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+            Console.WriteLine("Signed In!");
+        }
+
+        public void SignOut()
+        {
+            _authenticationState = SignedOutAuthenticationState;
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        }
+    }
+}
